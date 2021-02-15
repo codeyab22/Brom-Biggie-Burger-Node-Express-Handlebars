3cf7c496af42a97d57b19c090ca5f83da22a3210
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-02-15 12:26:33 -0500

    seeds.sql
    I am creating my names of the burgers that I want to try.

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured, date)
+VALUES
+(null, "Wave Burger", FALSE, null),
+(null, "Brocs Bowl Burger", FALSE, null),
+(null, "Quaker Burger", FALSE, null),
+(null, "ShuShu Shooter Burger", FALSE, null),
+(null, "Queenie Burger", FALSE, null),
+(null, "Mocha Mushroom Burger", FALSE, null);
