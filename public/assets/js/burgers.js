12bca88e600b5ca6e29152cad92f0260e73a49fe
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-04-26 22:27:47 -0400

    burgers.js
    I am going to give this an update.

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     // Add Burger to Database Button
   $("#add-burger").on("submit", function(event){
     event.preventDefault();
@@ -15,21 +14,15 @@ $(document).ready(function() {
       location.reload();
     });
   }); // End of Create New Burger
-  
   // Update Burger from Database Button
-
-  $(".burgerBlock").on("click", function(){
+  $("body").on("click", ".burgerBlock", function(){
     console.log("Button Clicked");
-
     const burgerID = $(this).data("id");
     const devoured = $(this).data("eaten");
-
     const burgerUpdate = {
       "devoured": devoured
     };
-
     console.log("button id is " + burgerID);
-
     $.ajax("/api/burger/" + burgerID, {
       type: "PUT",
       data: burgerUpdate
@@ -37,7 +30,5 @@ $(document).ready(function() {
       console.log("Burger Updated");
       location.reload();
     });
-
   }); // End of Devour it Button
-
 });
