803cb7e70f8cac85f6becace3219f0b351d7d4dd
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-04-26 23:07:04 -0400

    burgerModel.js
    I am going to give this an update.

--- models/burgerModel.js
+++ models/burgerModel.js
@@ -1,47 +1,25 @@
 const orm = require("../config/orm.js");
-
-
-
 /////////////////////////////////////////////// /*Model*/ //////////////////////////////////////////////////////////
-
 const burger = {
-
   selectAll : (callback) => {
-
    orm.selectAll((results)=>{ callback(results) 
    });
-
   },
-
   // The variables cols and vals are arrays.
   insertOne : ( vals, callback) => {
-
     // Execute orm Function to Post Data into Database
     orm.insertOne( vals, (result)=>{
       console.log("Executing Insert One Model");
       callback(result);
     });
-
   },
-
-  updateOne : ( vals, condition, callback) =>{
-
-
+  updateOne : (burgerID, callback) => {
     console.log("Executing Update One Model");
-
-    orm.updateOne( condition, (result)=>{
+    orm.updateOne(burgerID, (result)=>{
       console.log("Executing Second Declared CallBack");
       callback(result);
     });
-
   }
-
-
-
-
 }; // End of Burger Object
-
-
-
 /////////////////////////////////////////////// /*Exports*/ //////////////////////////////////////////////////////////
 module.exports = burger;
