bfec54b45b67903a47fc8545639c4cf9dbf54037
Adia Boyd <72044418+codeyab22@users.noreply.github.com>
2021-04-26 23:09:12 -0400

    orm.js
    I am going to change the orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,10 @@
 //Import (require) connection.js into orm.js
 var connection = require('../config/connection.js')
-
-
-
-
 //Create the methods that will execute the necessary MySQL commands in the controllers. 
 //These are the methods you will need to use in order to retrieve and store data in your database.
-
 var orm = 
 {
-
 	//selectAll()
-
 	selectAll: function(callback) 
 	{
 		//mySQL Query
@@ -21,7 +14,6 @@ var orm =
 			callback(result);
 		});
 	},
-
 	//insertOne()
 	insertOne: function(burger_name, callback)
 	{
@@ -33,13 +25,11 @@ var orm =
 				if (err) throw err;
 				callback(result);
 			});
-				
 	},
-
 	//updateOne()
 	updateOne: function(burgerID, callback)
 	{
-		connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: burgerID}],
+		connection.query('UPDATE burgers SET devoured = true WHERE id = ' + burgerID,
 			function(err, result)
 			{
 				if (err) throw err;
@@ -47,8 +37,5 @@ var orm =
 			});
 	}
 };
-
-
 // Export the ORM object in module.exports.
 module.exports = orm;
-
